0775d094ced49d2b25dda3ec0f28cfd80d048a36
NikkieS <21400389@handong.edu>
2020-04-08 18:24:40 +0900

    Update FirstJava.java

--- FirstJava/src/chapter1/FirstJava.java
+++ FirstJava/src/chapter1/FirstJava.java
@@ -4,9 +4,17 @@ public class FirstJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello~!! JAVA~!!");
+		/*System.out.println("Hello~!! JAVA~!!");
 		System.out.println("First Java program");
-
+		
+		int num;
+		
+		num = 10 + 20;
+		
+		System.out.println(num);
+		*/
+		System.out.println("2+5=" + 2+5); // 2+5 문자열로 인식
+		System.out.println("2+5=" + (2+5)); // 2+5 숫자열로 인식
 	}
 
 }
