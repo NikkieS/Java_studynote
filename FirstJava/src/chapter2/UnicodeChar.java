9fa7cbcbc4b76741b8160637dc35eed5b3205b4a
NikkieS <21400389@handong.edu>
2020-04-09 18:23:34 +0900

    유니코드로 char 생성

---
+++ FirstJava/src/chapter2/UnicodeChar.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class UnicodeChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch1 = 'A';
+		char ch2 = 'วั';
+		
+		System.out.println("ch1 = " + ch1);
+		System.out.println("ch2 = " + ch2);
+		
+
+		char ch3 = 65;
+		char ch4 = 54620;
+		System.out.println("ch3 = " + ch3);
+		System.out.println("ch4 = " + ch4);
+
+	}
+
+}
