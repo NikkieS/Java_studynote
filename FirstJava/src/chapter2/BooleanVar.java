30789e04c1f0d27eaf57df3f37a45f8fb8a32d6f
NikkieS <21400389@handong.edu>
2020-04-09 18:20:10 +0900

    boolean 연습

---
+++ FirstJava/src/chapter2/BooleanVar.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class BooleanVar {
+	public static void main(String[] args) {
+		boolean b1 = true ;
+		boolean b2 = false ;
+		
+		System.out.println(b1) ;
+		System.out.println(b2) ;
+		System.out.println("----------------------");
+		System.out.println(3<4) ;
+		System.out.println(3>4) ;
+		System.out.println("----------------------");
+		int num = 100;
+		System.out.println(num>4) ;
+	}
+}
