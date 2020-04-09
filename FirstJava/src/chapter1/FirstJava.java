1aa8380fe64b3432cf0a182b1811f79754fcb664
NikkieS <21400389@handong.edu>
2020-04-09 10:56:32 +0900

    Java print practice
    Java에서 출력하는 방법을 알아봄

--- FirstJava/src/chapter1/FirstJava.java
+++ FirstJava/src/chapter1/FirstJava.java
@@ -4,7 +4,7 @@ public class FirstJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*System.out.println("Hello~!! JAVA~!!");
+		System.out.println("Hello~!! JAVA~!!");
 		System.out.println("First Java program");
 		
 		int num;
@@ -12,9 +12,6 @@ public class FirstJava {
 		num = 10 + 20;
 		
 		System.out.println(num);
-		*/
-		System.out.println("2+5=" + 2+5); // 2+5 문자열로 인식
-		System.out.println("2+5=" + (2+5)); // 2+5 숫자열로 인식
 	}
 
 }
