cdf5c48dcc3e362281d0ab799c71b99e8b8e4820
NikkieS <21400389@handong.edu>
2020-04-09 18:21:55 +0900

    특수문자 출력 연습

---
+++ FirstJava/src/chapter2/SpecialChar.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class SpecialChar {
+	public static void main(String[] args) {
+		char single = '\''; // \' 따옴표 출력, single = '''; 와 같이 할 수 없다
+		String dblQuote = "\"Hello\""; // \" 겹따옴표를 출력
+		
+		String root = "c:\\"; // \\ 역슬래쉬 출력
+		System.out.println(single);
+		System.out.println(dblQuote);
+		System.out.println(root);
+	}
+
+}
