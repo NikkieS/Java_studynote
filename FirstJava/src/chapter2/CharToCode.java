bb6744a54d0e6e8dc0e470f8998c4e317ef105ff
NikkieS <21400389@handong.edu>
2020-04-09 18:21:44 +0900

    char 유니코드 변환

---
+++ FirstJava/src/chapter2/CharToCode.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class CharToCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'A'; // char ch = '\u0041' 로 바꿔 써도 같다
+		int code = ch;
+		
+		System.out.println("문자열 출력 : " + ch);
+		System.out.println("문자열 A의 유니코드 출력 : " + code);
+		
+		int code1 = 65; //int = 4byte
+		char ch1 = (char)code1; //char = 2byte
+		
+		System.out.println(code1);
+		System.out.println(ch1);
+		
+	}
+
+}
