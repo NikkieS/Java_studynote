1e482b27613bf8667cd2583e767b5332bfd292d7
NikkieS <21400389@handong.edu>
2020-04-10 14:55:24 +0900

    나눗셈 연습

---
+++ FirstJava/src/chapter3/DivOp.java
@@ -0,0 +1,19 @@
+package chapter3;
+
+public class DivOp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 7;
+		int num2 = 3;
+		
+		// 정수의 나누기 연산
+		System.out.println("정수의 나눗셈: " + num1/num2);
+		
+		// 실수의 나누기 연산
+		System.out.println("실수형 나눗셈: " + 7f/3f); // 리터럴로만 가능하다
+		System.out.println("실수의 나눗셈: " + (float)num1/num2);	//num2는 num1과 연산시 float화 해준다		
+		
+	}
+
+}
