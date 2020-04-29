aeea652c0dd03b353429a9357ebaf1471fd2f810
NikkieS <21400389@handong.edu>
2020-04-29 18:20:30 +0900

    Token_0429

---
+++ chapter13/src/ex/TokenTest.java
@@ -0,0 +1,21 @@
+package ex;
+
+import java.util.StringTokenizer;
+
+public class TokenTest {
+
+	public static void main(String[] args) {
+		String phoneNumber = "Tel 82-2-997-2059 test";
+		
+		StringTokenizer st1 = new StringTokenizer(phoneNumber);
+		while(st1.hasMoreTokens()) {
+			System.out.println(st1.nextToken());
+		}
+		System.out.println("--------------------------------------");
+		StringTokenizer st2 = new StringTokenizer(phoneNumber, " -", true);
+		while(st2.hasMoreTokens()) {
+			System.out.println(st2.nextToken());
+		}
+	}
+
+}
