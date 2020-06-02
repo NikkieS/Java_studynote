6509ab204b140c28ed9b98c096757b9aed4867f1
a4-01 <21400389@handong.edu>
2020-06-02 18:21:40 +0900

    Update DeptDao.java

--- chapter17_JDBC/src/prac/DeptDao.java
+++ chapter17_JDBC/src/prac/DeptDao.java
@@ -15,58 +15,33 @@ public class DeptDao {
 	// MVC = Model, View, Controller
 	// model : Service, DAO
 	// 데이터베이스 
-	Statement stmt = null;
-	PreparedStatement pstmt = null;
-	ResultSet rs = null;
 	
-	public void deptInsert(){	
-		System.out.println("-----------------------");
-		System.out.println("부서 정보를 입력해주세요.");
-		System.out.println("-----------------------");
-		System.out.println("부서 번호");
-		int deptno = kb.nextInt();
-		
-		System.out.println("부서 이름");
-		kb.nextLine();
-		String dname = kb.nextLine().toUpperCase();
-		
-		System.out.println("위치");
-		String loc = kb.nextLine().toUpperCase();
+	public int deptInsert(Dept dept){	
+		PreparedStatement pstmt = null;
+		Connection conn = null;
+		int resultCnt = 0;
 		
 		try {
+			conn = ConnProvider.getConnection();
 			//3. SQL 처리
 			String sql = "insert into dept (deptno, dname, loc) "
 					+ " values (?, ?, ?)";
 			
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, deptno);
-			pstmt.setString(2, dname);
-			pstmt.setString(3, loc);
 			
-			int resultCnt = pstmt.executeUpdate();
+			pstmt.setInt(1, dept.getDeptno());
+			pstmt.setString(2, dept.getDname());
+			pstmt.setString(3, dept.getLoc());
 			
-			if(resultCnt>0) {
-				System.out.println(resultCnt + "행이 정상적으로 입력되었습니다.");
-			} else {
-				System.out.println("입력이 되지않았습니다. 확인후 재 시도해주세요.");
-			}
+			resultCnt = pstmt.executeUpdate();
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
 			if(pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
@@ -75,60 +50,32 @@ public class DeptDao {
 				try {
 					conn.close();
 				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
 		}
+		return resultCnt;
 	}
 	
-	public void deptEdit(){
-		System.out.println("-----------------------");
-		System.out.println("수정하실 부서의 정보를 입력해주세요.");
-		System.out.println("-----------------------");
-		
-		System.out.println("찾으실 부서 이름");
-		kb.nextLine();
-		String dname = kb.nextLine().toUpperCase();
-		
-		System.out.println("수정하실 이름");
-		String editName = kb.nextLine().toUpperCase();
-		
-		System.out.println("부서 번호");
-		int deptno = kb.nextInt();
-		
-		System.out.println("위치");
-		kb.nextLine();
-		String loc = kb.nextLine().toUpperCase();
+	public int deptEdit(Dept newDept, Connection conn){
+		PreparedStatement pstmt = null;
+		int resultCnt = 0;
 		
 		try {
 			//3. SQL 처리
-			String sql = "update dept set deptno = ?, dname = ?, loc = ? where dname = '"+dname+"'";
+			String sql = "update dept set dname = ?, loc = ? where deptno = ?";
 			
 			pstmt = conn.prepareStatement(sql);
 			
-			pstmt.setInt(1, deptno);
-			pstmt.setString(2, editName);
-			pstmt.setString(3, loc);	
+			pstmt.setString(1, newDept.getDname());
+			pstmt.setString(2, newDept.getLoc());
+			pstmt.setInt(3, newDept.getDeptno());
 			
-			int resultCnt = pstmt.executeUpdate();
+			resultCnt = pstmt.executeUpdate();
 			
-			if(resultCnt>0) {
-				System.out.println("부서정보 "+resultCnt + "개가 정상적으로 수정되었습니다.");
-			} else {
-				System.out.println(dname+"에 대한 정보를 찾지 못하였습니다. 확인후 재 시도해주세요.");
-			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
 			if(pstmt != null) {
 				try {
 					pstmt.close();
@@ -137,53 +84,32 @@ public class DeptDao {
 					e1.printStackTrace();
 				}
 			}
-			
-			if(conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
 		}
+		return resultCnt;
 	}
 	
-	public void deptDelete() {
-		System.out.println("-----------------------");
-		System.out.println("삭제하실 부서의 번호를 입력해주세요.");
-		System.out.println("-----------------------");
-		
-		int deptno = kb.nextInt();
+	public int deptDelete(String dname) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int resultCnt = 0;
 		
 		try {
+			conn = ConnProvider.getConnection();
 			//3. SQL 처리
-			stmt = conn.createStatement();
-			String sql = "delete from dept where deptno ="+deptno;
+			String sql = "delete from dept where dname = ?";
 			
-			int resultCnt = stmt.executeUpdate(sql);
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dname);
 			
-			if(resultCnt>0) {
-				System.out.println(deptno+"의 부서정보가 정상적으로 삭제되었습니다.");
-			} else {
-				System.out.println("부서를 찾지 못하였습니다. 확인후 재 시도해주세요.");
-			}
+			resultCnt = pstmt.executeUpdate();
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
 			if(pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
@@ -192,11 +118,11 @@ public class DeptDao {
 				try {
 					conn.close();
 				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
 		}
+		return resultCnt;
 	}
 	
 	public List<Dept> deptList() {
@@ -273,14 +199,15 @@ public class DeptDao {
 		return deptList;
 	}
 	
-	public void deptSearch() {
-		System.out.println("-----------------------");
-		System.out.println("검색하실 부서의 이름을 입력해주세요.");
-		System.out.println("-----------------------");
-		kb.nextLine();
-		String dname = kb.nextLine().toUpperCase();
+	public List<Dept> deptSearch(String dname) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Connection conn = null;
+		
+		List<Dept> deptList = new ArrayList<>();
 		
 		try {
+			conn = ConnProvider.getConnection();
 			//3. SQL 처리
 			String sql = "select * from dept where dname like '%'||?||'%'";
 			
@@ -288,20 +215,16 @@ public class DeptDao {
 			pstmt.setString(1,  dname);
 			
 			rs = pstmt.executeQuery();
-			int resultCnt = 0;
 			
 			System.out.println("부서번호\t 부서이름\t\t  위치\t");
 			System.out.println("----------------------------------");		
 			
 			while(rs.next()) {
-				System.out.printf(rs.getInt("deptno") + "\t");
-				System.out.print(rs.getString("dname") + "\t");
-				System.out.print(rs.getString("loc") + "\n");
-				resultCnt++;
+				deptList.add(new Dept(rs.getInt("deptno"),
+									rs.getString("dname"),
+									rs.getString("loc")));
 			}
-			if(resultCnt<1) {
-				System.out.println("검색 결과가 없습니다.");
-			}
+			System.out.println("----------------------------------");	
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -332,5 +255,60 @@ public class DeptDao {
 				}
 			}
 		}
+		return deptList;
+	}
+	
+
+	public int deptSearchCount(String dname, Connection conn) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int rowCnt = 0;
+		
+		try {
+			conn = ConnProvider.getConnection();
+			
+			String sql = "select count(*) from dept where dname=?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, dname);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				rowCnt=rs.getInt(1);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return rowCnt;
+	}
+	
+	public Dept deptSearchName(String dname, Connection conn) {
+		Dept dept = null;
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = ConnProvider.getConnection();
+			
+			String sql = "select * from dept where dname = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, dname);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dept = new Dept(rs.getInt(1),
+								rs.getString(2),
+								rs.getString(3));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return dept;
 	}
 }
